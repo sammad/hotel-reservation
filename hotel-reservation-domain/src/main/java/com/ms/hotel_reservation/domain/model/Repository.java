5652ed4dc741fc52ab4d24bb0b762f00b112cf74
sammad <sambharmadhur@gmail.com>
2020-02-26 10:30:00 +0530

    Removed domain

--- hotel-reservation-domain/src/main/java/com/ms/hotel_reservation/domain/model/Repository.java
+++
@@ -1,8 +0,0 @@
-package com.ms.hotel_reservation.domain.model;
-
-public interface Repository<ET, T> extends ReadOnlyRepository<ET, T> {
-
-	public void add(ET entity);
-	public void update(ET entity);
-	public boolean delete(T id);
-}
